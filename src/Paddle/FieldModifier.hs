e4e8ec1af3196d9022b93978543b768ac652474f
Domen Kožar <domen@dev.si>
2023-04-17 11:19:32 +0100

    remove warning

--- src/Paddle/FieldModifier.hs
+++ src/Paddle/FieldModifier.hs
@@ -6,7 +6,6 @@ module Paddle.FieldModifier
 import Data.Aeson
 import Protolude
 import Prelude ()
-import           Data.Char                      (toLower, isUpper)
 
 modifier :: [Char] -> [Char]
 modifier = concatMap (\x -> if isUpper x then ['_', toLower x] else [x])
